016dbacf1be626f635fceaae239352297f084c00
Roman Fihura <romanfigura120@gmail.com>
2021-04-15 21:31:14 +0300

    upddiscard

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@
 int main() {
   rolling_buffer test;
 
-  for (uint32_t i = 0; i < 11; i++) {
+  for (uint32_t i = 1; i < 11; i++) {
     test.push(i);
   }
- 
-  std::cout << test.at(5);
+
+  // std::cout << test.at(5);
+  test.push(5555);
+  // test.push(666);
+  test.show();
   std::cout << std::endl << test.size();
   return 0;
 }
--- rollingbuffer.h
+++ rollingbuffer.h
@@ -1,19 +1,27 @@
 #pragma once
+#include <iostream>
 #include <iterator>
 #include <stdint.h>
 #include <vector>
 
 class rolling_buffer {
 public:
-  rolling_buffer(uint32_t size = 10)
+  rolling_buffer(uint32_t size = 9)
       : data(size), current_position_iterator{data.begin()} {}
+
   uint32_t push(uint32_t element) {
     if (current_position_iterator == data.end()) {
       discard();
     }
     *current_position_iterator = element;
+    //data.push_back(element);
     current_position_iterator++;
     return element;
+  }
+  void show() {
+    for (uint32_t i = 0; i < data.size(); i++) {
+      std::cout << data[i] << std::endl;
+    }
   }
   uint32_t size() {
     return std::distance(data.begin(), current_position_iterator);
@@ -24,10 +32,11 @@ private:
   std::vector<int> data;
   std::vector<int>::iterator current_position_iterator;
   void discard() {
+
+    data.erase(data.begin(), data.begin() + data.capacity() / 2);
     current_position_iterator =
         data.begin() + data.capacity() / 2 + data.capacity() % 2;
-    data.erase(data.begin(), data.begin() + data.capacity() / 2);
-    data.resize(10);
+    data.resize(data.capacity());
     // std::copy(data.begin() + data.size() / 2, data.end(), data.begin());
   }
 };
